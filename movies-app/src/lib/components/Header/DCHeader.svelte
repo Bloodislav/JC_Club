8e2e5dd114f74e8a02c9e158a7658e246882abce
Bloodislav <vladhamrik@gmail.com>
2023-11-15 19:56:01 +0500

    Init movie-app

---
+++ movies-app/src/lib/components/Header/DCHeader.svelte
@@ -0,0 +1,10 @@
+<header class="header">
+    1
+</header>
+
+<style>
+    .header{
+        background: url('/Images/header-bg.png') no-repeat center;
+        height: 600px;
+    }
+</style>
---
+++ movies-app/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../assets/app.css";
+    import DcHeader from "$lib/components/Header/DCHeader.svelte";
+</script>
+
+<DcHeader />
+<!-- Все страниц(элементы) после lyout будут попадать в <slot />-->
+<slot />
+<div>Footer</div>
